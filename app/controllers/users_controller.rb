4486c96e212eeeea1127c046cfadc79e65f84357
suzukimit <mtr.suzuki@gmail.com>
2015-12-23 06:59:17 +0000

    Add profile setting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update]
   
   def show
-    @user = User.find(params[:id])
   end
   
   def new
@@ -18,9 +18,25 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Updated your profile!"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :profile, :area, :password, :password_confirmation)
+  end
+  
+  def find_user
+    @user = User.find(params[:id])
   end
 end
